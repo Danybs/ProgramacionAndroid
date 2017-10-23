cdd547e88d7f9385066a846c0933f37d1ac5f3e0
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-23 17:47:27 +0200

    empezandoListView

---
+++ applistview/src/main/java/com/example/applistview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.applistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] contacto = { "Pepe", "Maria", "Luis", "Maite", "Paco"};
+    private String[] telefono = { "620 34 56 78", "631 98 45 35", "650 75 68 20", "633 45 84 29", "666 36 27 18"};
+
+    private TextView tv1;
+    private ListView lv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1=(TextView)findViewById(R.id.tv1);
+        lv1=(ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, contacto);
+
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}

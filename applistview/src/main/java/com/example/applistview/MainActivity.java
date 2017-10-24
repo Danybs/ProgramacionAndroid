fe6298bcdbeb62265fca3571b0ada873de1f8129
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-24 19:04:19 +0200

    funcionando

--- applistview/src/main/java/com/example/applistview/MainActivity.java
+++ applistview/src/main/java/com/example/applistview/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] contacto = { "Pepe", "Maria", "Luis", "Maite", "Paco"};
-    private String[] telefono = { "620 34 56 78", "631 98 45 35", "650 75 68 20", "633 45 84 29", "666 36 27 18"};
+    private String[] contacto = {"Pepe", "Maria", "Luis", "Maite", "Paco"};
+    private String[] telefono = {"620 34 56 78", "631 98 45 35", "650 75 68 20", "633 45 84 29", "666 36 27 18"};
 
     private TextView tv1;
     private ListView lv1;
@@ -20,16 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv1=(TextView)findViewById(R.id.tv1);
-        lv1=(ListView)findViewById(R.id.lv1);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        lv1 = (ListView) findViewById(R.id.lv1);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, contacto);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacto);
 
         lv1.setAdapter(adapter);
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+            public void onItemClick(AdapterView<?> parent, View v, int posicion, long id) {
+                tv1.setText("Telefono de " + lv1.getItemAtPosition(posicion)
+                        + " es " + telefono[posicion]);
             }
         });
     }

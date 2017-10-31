6a4fbe06b86e46540b50773a0e11e663b8d86892
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-31 18:59:31 +0100

    eventos

---
+++ app/src/main/java/com/example/alumno/passwordapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.alumno.passwordapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dani.programacionandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
